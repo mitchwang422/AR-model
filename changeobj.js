fc6c4cd6c249f1d9304f95789a2490a551ad25ae
mitchwang422 <34224967+mitchwang422@users.noreply.github.com>
2018-04-15 23:47:50 +1200

    Update changeobj.js

--- changeobj.js
+++ changeobj.js
@@ -1,12 +1,12 @@
 function changemodel( title )
 {
 	console.log("changemode") 
-  	if (title === "bigmax")
-  	{
-  		console.log("Hello world!" + title  ) ;
-//   		document.getElementByID(obj).src="bigmax/model.obj"
-//        document.getElementByID(mtl).src="bigmax/model.mtl"
-  	}
+//   	if (title === "bigmax")
+//   	{
+//   		console.log("Hello world!" + title  ) ;
+// //   		document.getElementByID(obj).src="bigmax/model.obj"
+// //        document.getElementByID(mtl).src="bigmax/model.mtl"
+//   	}
 
        
 }
