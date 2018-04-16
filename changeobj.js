52f374b4182a1db6945fd143d29c8861fcfdd8a1
mitchwang422 <34224967+mitchwang422@users.noreply.github.com>
2018-04-16 17:30:44 +1200

    Update changeobj.js

--- changeobj.js
+++ changeobj.js
@@ -1,12 +1,12 @@
-function changemodel()
+function changemodel(title)
 {
-	console.log("Hello world!") ;
-	
-	 if (scan.content === "bigmax")
+
+	console.log("changemode") 
+  	if (title == "bigmax" )
   	{
   		console.log("Hello world!" + title  ) ;
-		document.getElementByID(obj).src="bigmax/model.obj"
-                document.getElementByID(mtl).src="bigmax/model.mtl"
+  		document.getElementByID(obj).src="bigmax/model.obj"
+        document.getElementByID(mtl).src="bigmax/model.mtl"
   	}
-       
+
 }
