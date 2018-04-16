44cbda6eb5ab32b8937905bb3e7c77008a40b8b2
mitchwang422 <34224967+mitchwang422@users.noreply.github.com>
2018-04-17 11:28:16 +1200

    Update changeobj.js

--- changeobj.js
+++ changeobj.js
@@ -1,12 +1,34 @@
-function changemodel(title)
+function workOrderStatus( variable ){
+
+    if (variable === "bigmax" )
 {
+	console.log('variable = '+ variable) ;
+ 
+		$('#obj').attr('src' , "bigmax/model.obj") ;
+		$('#mtl').attr('src' , "bigmax/model.mtl") ;	
 
-	console.log("changemode") 
-  	if (title == "bigmax" )
-  	{
-  		console.log("Hello world!" + title  ) ;
-  		document.getElementByID(obj).src="bigmax/model.obj"
-        document.getElementByID(mtl).src="bigmax/model.mtl"
-  	}
 
-}
+		document.querySelector('a-entity').components.position.flushToDOM();  // Flush a component.
+		document.querySelector('a-entity').flushToDOM();  // Flush an entity.
+		document.querySelector('a-entity').flushToDOM(true);  // Flush an entity and its children.
+		document.querySelector('a-scene').flushToDOM(true);  // Flush every entity.
+		alert('obj = ' +  $('#obj').attr('src')  +' mtl =' + $("#mtl").attr('src')) ;
+
+  }  
+
+    else if (variable === "skull" )
+{
+	console.log('variable = '+ variable) ;
+
+		$('#obj').attr('src' , "skull/model.obj") ;
+		$('#mtl').attr('src' , "skull/model.mtl") ;	
+
+
+		document.querySelector('a-entity').components.position.flushToDOM();  // Flush a component.
+		document.querySelector('a-entity').flushToDOM();  // Flush an entity.
+		document.querySelector('a-entity').flushToDOM(true);  // Flush an entity and its children.
+		document.querySelector('a-scene').flushToDOM(true);  // Flush every entity.
+		alert('obj = ' +  $('#obj').attr('src')  +' mtl =' + $("#mtl").attr('src')) ;
+
+  }  
+
